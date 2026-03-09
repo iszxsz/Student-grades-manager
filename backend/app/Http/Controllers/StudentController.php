c63fcd7a897af260bc316992f3025a0660bc3492
Isabela Florêncio <isafflorencio@gmail.com>
2026-03-09 20:48:38 -0300

    feat: implement student CRUD

---
+++ backend/app/Http/Controllers/StudentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller{
+    public function index(){
+        
+        return response()->json(Student::all());
+    }
+
+    public function store(Request $request){
+        $student = Student::create($request->all());
+        return response()->json($student, 201);
+    }
+
+    public function show($id){
+        $student = Student::findOrFail($id);
+        return response()->json($student);
+    }
+
+    public function update(Request $request, $id){
+        $student = Student::findOrFail($id);
+        $student->update($request->all());
+        return response()->json($student);
+    }
+
+    public function destroy($id){
+        Student::destroy($id);
+        return response()->json(null, 204);
+    }
+}
