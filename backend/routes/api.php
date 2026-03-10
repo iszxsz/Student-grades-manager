c2be33c89c00676b99a2c83d8e0fe69a8c0418df
Isabela Florêncio <isafflorencio@gmail.com>
2026-03-09 22:34:54 -0300

    feat: add API routes for students

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
 Route::apiResource('students', StudentController::class);
+Route::get('dashboard', [StudentController::class, 'dashboard']);
+Route::get('class-average-grades', [StudentController::class, 'classAverageGradesPerSubject']);
+Route::get('class-average-grade', [StudentController::class, 'getClassAverageGrade']);
+Route::get('students-with-low-attendance', [StudentController::class, 'getStudentsWithLowAttendance']);
+Route::get('top-performing-students', [StudentController::class, 'getTopPerformingStudents']);
